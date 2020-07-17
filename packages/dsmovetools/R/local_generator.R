1697e22cf25a0c96c479835e49f03a4ec1ebfef7
Josh Hewitt <joshua.hewitt@duke.edu>
2020-07-17 10:29:03 -0600

    allowing for creation of non-square generator matrices

--- packages/dsmovetools/R/local_generator.R
+++ packages/dsmovetools/R/local_generator.R
@@ -3,7 +3,8 @@
 #' @name local_generator
 #'
 #' @param locs vector of locations associated with \code{edges}
-#' @param edges vector of edges to build generator matrix for
+#' @param row_edges vector of edges to build generator matrix for
+#' @param col_edges vector of edges to build generator matrix for
 #' @param tolocs_by_edge location at which each edge in domain ends
 #' @param fromlocs_by_edge location from which each edge in domain begins
 #' @param outedges_by_loc edges in domain sorted by their starting location
@@ -29,33 +30,40 @@ NULL
 #' @rdname local_generator
 #' 
 local_generator = nimbleFunction(
-  run = function(locs = integer(1), edges = integer(1), 
-                 tolocs_by_edge = integer(1), fromlocs_by_edge = integer(1),
-                 outedges_by_loc = integer(1), loc_start = integer(1), 
-                 Xloc = double(2), betaLoc = double(1), Xdir = double(2), 
-                 betaDir = double(1), W = double(2), betaAR = double(0)) {
+  run = function(locs = integer(1), row_edges = integer(1), 
+                 col_edges = integer(1), tolocs_by_edge = integer(1), 
+                 fromlocs_by_edge = integer(1), outedges_by_loc = integer(1), 
+                 loc_start = integer(1), Xloc = double(2), betaLoc = double(1), 
+                 Xdir = double(2), betaDir = double(1), W = double(2), 
+                 betaAR = double(0)) {
     
     # return an infinitesimal generator matrix
     returnType(double(2))
     
     nlocs <- length(locs)
-    nedges <- length(edges)
+    nrows <- length(row_edges)
+    ncols <- length(col_edges)
     
     # zero-initialize local infinitesimal generator matrix
-    A <- matrix(0, nrow = nedges, ncol = nedges)
+    A <- matrix(0, nrow = nrows, ncol = ncols)
     
     # pre-compute common, location-based drivers of movement
     lambda_loc <- exp((Xloc[locs,] %*% betaLoc)[,1])
     
     # loop over matrix rows
-    for(i in 1:nedges) {
-      edge_i <- edges[i]
+    for(i in 1:nrows) {
+      edge_i <- row_edges[i]
       
       # get location at which edge_i ends
       to_loc <- tolocs_by_edge[edge_i]
       
+      # row_edges and col_edges may have different ordering; determine where 
+      # edge_i exists in col_edges, so we can store the infinitesimal 
+      # transition rate in the correct location
+      identity_column <- intWhich(x = edge_i, vec = col_edges)
+      
       # set infinitesimal rate at which edge_i is being left
-      A[i,i] <- - lambda_loc[intWhich(x = to_loc, vec = locs)]
+      A[i, identity_column] <- - lambda_loc[intWhich(x = to_loc, vec = locs)]
       
       # initialize off-diagonal mass counter
       offdiagMass <- 0
@@ -67,10 +75,10 @@ local_generator = nimbleFunction(
       for(j in loc_start[to_loc]:(loc_start[to_loc+1]-1)) {
         edge_j <- outedges_by_loc[j]
         
-        # restrict to off-diagonal entries for A
+        # restrict to "off-diagonal" entries for A
         if(edge_i != edge_j) {
           # process out-edge edge_j if it has a column in our matrix
-          if(intContains(x = edge_j, vec = edges)) {
+          if(intContains(x = edge_j, vec = col_edges)) {
             dst_loc <- tolocs_by_edge[edge_j]
             
             # unstandardized infinitesimal rate for transition to_loc -> dst_loc
@@ -82,7 +90,7 @@ local_generator = nimbleFunction(
             )
             
             # set unstandardized rate at which edge_i is transitioning to edge_j
-            A[i,intWhich(x = edge_j, vec = edges)] <- lambda
+            A[i,intWhich(x = edge_j, vec = col_edges)] <- lambda
             
             # aggregate unstandardized off-diagonal mass
             offdiagMass <- offdiagMass + lambda
@@ -91,10 +99,10 @@ local_generator = nimbleFunction(
       }
       
       # standardize off-diagonal entries so that the rowsum is 0
-      for(j in 1:nedges) {
-        if(i!=j) {
+      for(j in 1:ncols) {
+        if(j!=identity_column) {
           if(A[i,j] != 0) {
-            A[i,j] <- - A[i,j] / offdiagMass * A[i,i]
+            A[i,j] <- - A[i,j] / offdiagMass * A[i, identity_column]
           }
         }
       }
