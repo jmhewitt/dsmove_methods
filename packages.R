54e4ea90ae27c5be1365d01771f6d7eb06eb32b8
Josh Hewitt <joshua.hewitt@duke.edu>
2020-08-25 10:10:13 -0600

    Update packages.R

--- packages.R
+++ packages.R
@@ -3,7 +3,14 @@ library(dotenv)
 library(drake)
 
 devtools::document('packages/dsmovetools/')
+library(ggplot2)
+library(ggthemes)
 library(igraph)
+library(metR)
 library(nimble)
 library(sp)
 library(spdep)
+library(viridis)
+
+# compile likelihood function
+cctds_nbhd_ll = compileNimble(ctds_nbhd_ll)
