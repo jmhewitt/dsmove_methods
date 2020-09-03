050ecea3e279555b66c2d51bfb86d1cf1e9be464
Josh Hewitt <joshua.hewitt@duke.edu>
2020-09-03 15:14:21 -0600

    re-order simulation observation times

--- R/simulation/observe_ctds.R
+++ R/simulation/observe_ctds.R
@@ -3,12 +3,17 @@ observe_ctds = function(sim_trajectory, n_obs, sim_dir) {
   # load simulated trajectory
   ctds_sim = readRDS(sim_trajectory)
   
+  # tseq = seq(from = ctds_sim$times[1], 
+  #            to = ctds_sim$times[length(ctds_sim$times)], 
+  #            length.out = n_obs)
+  
+  tseq = sort(runif(n = n_obs, min = ctds_sim$times[1], 
+               max = ctds_sim$times[length(ctds_sim$times)]))
+  
   # observe trajectory
   ctds_obs = ctds.observe(
     states = ctds_sim$states, times = ctds_sim$times, 
-    t.obs = seq(from = ctds_sim$times[1], 
-                to = ctds_sim$times[length(ctds_sim$times)], 
-                length.out = n_obs))
+    t.obs = tseq)
   
   # save object
   f = file.path(sim_dir, paste(id_chr(), '.rds', sep = ''))
