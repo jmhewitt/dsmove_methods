4fba79af72e2c756003b3a56a626fadf24664e1b
Josh Hewitt <joshua.hewitt@duke.edu>
2020-09-03 15:16:01 -0600

    plot truth against imputations/estimates for simulation

--- R/subplans/simulation_plan.R
+++ R/subplans/simulation_plan.R
@@ -28,6 +28,23 @@ simulation_plan = drake_plan(
     format = 'file'
   ),
   
+  # look at average duration in simulated trajectory
+  sim_avg_duration = {
+    ctds_sim = readRDS(sim_trajectory)
+    mean(ctds_sim$durations)
+  },
+  
+  # plot simulation
+  sim_plot = {
+    ctds_sim = readRDS(sim_trajectory)
+    colnames(sim_domain$coords) = c('x', 'y')
+    sim_domain$coords = data.frame(sim_domain$coords)
+    pl = plot(x = ctds_sim, ctds_struct = sim_domain) + 
+      ggtitle('Simulated trajectory')
+    ggsave(pl, filename = file.path(sim_plots, 
+                                    paste(id_chr(), '.png', sep = '')))
+  },
+  
   # observe simulated trajectory
   sim_obs = target(
     observe_ctds(sim_trajectory, n_obs, sim_dir),
@@ -38,18 +55,75 @@ simulation_plan = drake_plan(
   # plot likelihood surface
   sim_lik_surface = target(
     plot_obs_lik(ctds_struct = sim_domain, obs = sim_obs, plot_dir = sim_plots,
-                 # beta_loc_seq = seq(from = -1.5, to = 2.5, length.out = 25),
-                 # beta_ar_seq = seq(from = -1, to = 5, length.out = 25),
-                 beta_loc_seq = seq(from = 2, to = 8, length.out = 25),
-                 beta_ar_seq = seq(from = -1, to = 1, length.out = 25),
+                 beta_loc_seq = seq(from = -1.5, to = 2.5, length.out = 25),
+                 beta_ar_seq = seq(from = -1, to = 5, length.out = 25),
+                 # beta_loc_seq = seq(from = 2, to = 8, length.out = 25),
+                 # beta_ar_seq = seq(from = -1, to = 1, length.out = 25),
                  beta_loc, beta_ar),
     dynamic = map(sim_obs), 
     max_expand = 1
   ),
   
+  # fit model using crawl approximation
   sim_fit_hanks = target(
-    fit_hanks(ctds_struct = sim_domain, obs = sim_obs),
+    fit_hanks(ctds_struct = sim_domain, obs = sim_obs, plot_dir = sim_plots),
     dynamic = map(sim_obs)
-  )
+  ),
+  
+  # assemble crawl approximation results across timesteps
+  sim_fit_hanks_summary = target(
+    summarize_fit_hanks(fit = sim_fit_hanks, beta_loc = beta_loc, 
+                        beta_ar = beta_ar),
+    dynamic = map(sim_fit_hanks)
+  ),
+  
+  plot_hanks_summary = {
+    pl = ggplot(sim_fit_hanks_summary, 
+           aes(x = tstep, y = Estimate, ymin = lwr, ymax = upr)) + 
+      geom_pointrange() + 
+      geom_hline(mapping = aes(yintercept = Estimate), 
+                 data = data.frame(
+                   param = c('(Intercept)', 'crw'),
+                   Estimate = c(beta_loc, beta_ar)), 
+                 lty = 3) + 
+      xlab('Time between observations') +
+      ggtitle('Hanks recovery of true parameters (Truth at dotted line)') + 
+      facet_wrap(~param) +
+      theme_few()
+    
+    ggsave(pl, filename = file.path(sim_plots, 
+                                    paste(id_chr(), '.pdf', sep = '')))
+      
+  },
+  
+  # compare imputed trajectories to truth
+  imputed_plots = target(
+    plot_imputations(ctds_struct = sim_domain, trajectory = sim_trajectory,
+                     imputations = sim_fit_hanks$ctmc.list,
+                     raster.coords = sim_fit_hanks$raster.coords,
+                     tstep = sim_fit_hanks$tstep,
+                     plot_dir = sim_plots),
+    dynamic = map(sim_fit_hanks)
+  ),
+  
+  # fit model to completely observed trajectory
+  sim_fit_exact = target(
+    fit_exact(ctds_struct = sim_domain, trajectory = sim_trajectory, 
+              beta_loc = beta_loc, beta_dir = beta_dir, beta_ar = beta_ar)
+  ),
+  
+  # assemble exact fit results
+  sim_fit_exact_summary = 
+    data.frame(
+      param = c('beta_loc', 'beta_ar'), 
+      truth = c(beta_loc, beta_ar),
+      est = sim_fit_exact$par[-2],
+      se = sqrt(diag(solve(-sim_fit_exact$hessian[-2,-2])))
+    ) %>% 
+    dplyr::mutate(
+      lwr = est - 1.96 * se,
+      upr = est + 1.96 * se,
+      covered = (lwr <= truth) & (truth <= upr)
+    )
   
 )
