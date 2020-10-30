df1a353c8471081e38ac1ead4691116e0e306f32
Josh Hewitt <joshua.hewitt@duke.edu>
2020-10-30 14:50:44 -0600

    extract portion of ctds trajectory falling within a window

---
+++ R/approx_integration/extract_segment.R
@@ -0,0 +1,21 @@
+extract_segment = function(epath, tpath, tmin, tmax) {
+  # Extract the portion of the complete trajectory that covers the time range 
+  # [tmin, tmax].
+  # 
+  # Parameters:
+  #  epath - edges visited in complete trajectory
+  #  tpath - transition times in complete trajectory
+  
+  # indices of transitions that immediately precede and follow [tmin, tmax]
+  istart = max(which(tpath <= tmin))
+  iend = min(which(tmax <= tpath))
+  
+  # correct end index if last path transition occurs before tmax
+  if(is.infinite(iend)) {
+    iend = length(tpath)
+  }
+  
+  # extract and return trajectory segment that covers [tmin, tmax]
+  inds = istart:iend
+  list(epath = epath[inds], tpath = tpath[inds])
+}
