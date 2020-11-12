faa4e318931b470cf9c28dc22b8efb55556aac9c
Josh Hewitt <joshua.hewitt@duke.edu>
2020-11-12 14:55:37 -0700

    add checkpoints, priors, and initialization to gibbs sampler

--- R/approx_integration/fit_integration.R
+++ R/approx_integration/fit_integration.R
@@ -1,4 +1,5 @@
-fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
+fit_integration = function(segments, obs, inits, priors, niter, ctds_domain,
+                           ncheckpoints, output_dir) {
   
   # extract sampling weights and indexes for all segments
   pathwts = lapply(segments, function(sfam) {
@@ -10,43 +11,17 @@ fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
   })
   
   #
-  # initialize data augmentation path
+  # read in initial data augmentation path
   #
   
-  path_components = vector('list', length(segments) + 1)
-  
-  # randomly sample an initial edge, and set initial time
-  in_edges = ctds_domain$in_edges_inds[[obs$states[1]]]
-  path_components[[1]]$path_ind = ifelse(
-    length(in_edges) > 1, sample(x = 1:length(in_edges), size = 1), 1
-  )
-  path_components[[1]]$epath = in_edges[path_components[[1]]$path_ind]
-  path_components[[1]]$tpath = obs$times[1]
-  
-  # sample path segments
-  for(i in 2:length(path_components)) {
-    p = pathwts[[i-1]]
-    # sample path segment
-    # seg_ind = ifelse(nrow(p) > 1, sample(x = nrow(p), size = 1, prob = p$w), 1)
-    seg_ind = 1
-    path_components[[i]]$path_ind = seg_ind
-    # extract path
-    r = p[seg_ind, ]
-    path_components[[i]]$epath = segments[[i-1]][[r$lengthind]]$paths[r$pathind,]
-    # sample times
-    path_components[[i]]$tpath = sort(runif(
-      n = length(path_components[[i]]$epath), 
-      min = obs$times[i-1], 
-      max = obs$times[i]
-    ))
-  }
+  path_components = inits$path_components
   
   
   #
   # sampler
   #
   
-  # initialize state
+  # read in initial parameters
   param_vec = c(inits$beta_ar, inits$beta_loc)
   
   # initialize output
@@ -68,35 +43,41 @@ fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
   
   # wrapper to evaluate log-posterior 
   lpfn = function(theta, epath, durations) {
+    # likelihood
     ll_exact(epath = epath, durations = durations,
              beta_loc = matrix(theta[-1], ncol = 1), beta_dir = 0,
-             beta_ar = theta[1], ctds_struct = ctds_domain)
+             beta_ar = theta[1], ctds_struct = ctds_domain) + 
+    # prior
+    dnorm(x = theta[1], mean = priors$beta_ar['mean'], 
+          sd = priors$beta_ar['sd'], log = TRUE) + 
+      dnorm(x = theta[-1], mean = priors$beta_loc['mean'], 
+            sd = priors$beta_loc['sd'], log = TRUE)
   }
-  
-  # posterior mode for initial conditions
-  o = optim(par = param_vec, function(theta) {
-    lpfn(theta = theta, epath = epath, durations = diff(tpath))
-  }, control = list(fnscale = -1), method = 'BFGS', hessian = TRUE)
-  
-  # update initial parameters
-  param_vec = o$par
   
   # construct MHRW sampler
   paramSampler = MhrwAdaptive$new(
-    x = param_vec, mu = o$par, Sigma = solve(-o$hessian), lambda = c(1, 1), 
-    lp = lpfn, C = .1, alpha = 1, alpha_star = .44, adaptive = TRUE, 
-    adaptation_frequency = 1
+    x = param_vec, mu = param_vec, Sigma = inits$prop_cov, 
+    lambda = c(1, 1), lp = lpfn, C = .1, alpha = 1, alpha_star = .44, 
+    adaptive = TRUE, adaptation_frequency = 1
   )
   
-  # compute initial likelihood
-  ll = lpfn(theta = param_vec, epath = epath, durations = diff(tpath))
+  # initialize data likelihood
+  ll = ll_exact(epath = epath, durations = diff(tpath),
+                beta_loc = matrix(param_vec[-1], ncol = 1),
+                beta_dir = 0, beta_ar = param_vec[1],
+                ctds_struct = ctds_domain)
   
+  # set checkpoint indices
+  checkpoint.inds = seq(from = 1, to = niter, length.out = ncheckpoints)[-1]
+  
+  # set file output
+  f = file.path(output_dir, paste(id_chr(), '.rds', sep = ''))
+    
   for(it in 1:niter) {
     
     # update model parameters
     update = paramSampler$sample(epath =  epath, durations = diff(tpath))
     param_vec = update$x
-    ll = update$lp
     
     #
     # update path
@@ -106,16 +87,6 @@ fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
     
     # sample path segments
     for(i in 2:length(path_components)) {
-
-      # # flatten current path, and get ll for segment
-      # epath_seg = do.call(c, sapply(path_components[(i-1):(i+1)], function(p) {
-      #   unlist(p$epath)
-      # }))
-      # tpath_seg = do.call(c, sapply(path_components[(i-1):(i+1)],
-      #                               function(p) { p$tpath }))
-      # ll_seg = ll_exact(epath = epath_seg, durations = diff(tpath_seg),
-      #                   beta_loc = matrix(param_vec[-1], ncol = 1), beta_dir = 0,
-      #                   beta_ar = param_vec[1], ctds_struct = ctds_domain)
 
       # propose new path segment
       p = pathwts[[i-1]]
@@ -150,40 +121,16 @@ fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
         x_prop = extract_segment(epath = epath_prop, tpath = tpath_prop, 
                                  tmin = obs$times[i-1], tmax = obs$times[i])
         
-        if(any(length(x0$tpath) == 1,
-               length(x_prop$tpath) == 1)) {
-          browser()
-        }
-        
         # compute likelihood on original and changed path segments
-        ll0 = ll_exact(epath = x0$epath, durations = diff(x0$tpath),
-                       beta_loc = matrix(param_vec[-1], ncol = 1),
-                       beta_dir = 0, beta_ar = param_vec[1],
-                       ctds_struct = ctds_domain)
-        ll_prop = ll_exact(epath = x_prop$epath, durations = diff(x_prop$tpath),
-                           beta_loc = matrix(param_vec[-1], ncol = 1),
-                           beta_dir = 0, beta_ar = param_vec[1],
-                           ctds_struct = ctds_domain)
-        
-        # # compute likelihood only on changed portion of path
-        # ll_prop = ll_exact(epath = epath_prop, durations = diff(tpath_prop),
-        #                    beta_loc = matrix(param_vec[-1], ncol = 1),
-        #                    beta_dir = 0, beta_ar = param_vec[1],
-        #                    ctds_struct = ctds_domain)
-        # 
-        # 
-        # 
-        # # accept/reject
-        # logR = ll_prop - ll +
-        #   log(pathwts[[i-1]]$w[path_components[[i]]$path_ind]) -
-        #   log(pathwts[[i-1]]$w[prop_components$path_ind])
+        ll0 = lpfn(theta = param_vec, epath = x0$epath, 
+                   durations = diff(x0$tpath))
+        ll_prop = lpfn(theta = param_vec, epath = x_prop$epath, 
+                       durations = diff(x_prop$tpath))
         
         # accept/reject 
         logR = ll_prop - ll0 + 
           log(pathwts[[i-1]]$w[path_components[[i]]$path_ind]) -
           log(pathwts[[i-1]]$w[prop_components$path_ind])
-        
-        
         accept = log(runif(n = 1)) < logR
         
         if(accept) {
@@ -197,8 +144,6 @@ fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
         }
         
       }
-      
-      
 
     }
     
@@ -207,22 +152,16 @@ fit_integration = function(segments, obs, inits, priors, niter, ctds_domain) {
     samples$param_vec[it, ] = param_vec
     samples$ll[it] =  ll
     
-    # # maximum possible length of trajectories
-    # sum(unlist(sapply(segments, function(s) {
-    #   ncol(s[[length(s)]]$paths)
-    # })))
-    # 
-    # # length of sampled trajectories
-    # plot(sapply(samples$path, function(p) {
-    #   nrow(p)
-    # })[-(1:60)], type = 'l', ylab = 'path len')
-    # 
+    # checkpoint operations
+    if(it %in% checkpoint.inds) {
+      saveRDS(samples, file = f)
+    }
     
-    message(ll)
-    print(param_vec)
   }
   
-  browser()
+  # save final samples
+  saveRDS(samples, file = f)
   
-  samples
+  # return output file name
+  f
 }
