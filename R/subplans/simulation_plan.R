af4e2ee4b4b5cda7a1a125faf0fd96f3ab1f38c0
Josh Hewitt <joshua.hewitt@duke.edu>
2020-11-24 11:23:09 -0700

    loosening beta_loc prior

--- R/subplans/simulation_plan.R
+++ R/subplans/simulation_plan.R
@@ -45,7 +45,7 @@ simulation_plan = drake_plan(
   prior_params = target(
     list(
       beta_ar = list(mean = 0, sd = 1e2),
-      beta_loc = list(mean = sim_params_1$beta_loc[1], sd = 1e-8)
+      beta_loc = list(mean = sim_params_1$beta_loc[1], sd = 1e-4)
     ),
     hpc = FALSE
   ),
