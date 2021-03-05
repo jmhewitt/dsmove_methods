f30ac02b56da5869f78f89f99b1bd6d469e967b6
jmhewitt <joshua.hewitt@duke.edu>
2021-03-05 16:52:34 -0700

    refactor rookneighborhood to allow subclasses that use more complex domains

--- packages/dsmovetools/src/RookNeighborhood.h
+++ packages/dsmovetools/src/RookNeighborhood.h
@@ -15,7 +15,7 @@
 template<typename size_type, typename Index>
 class RookNeighborhood {
 
-    private:
+    protected:
 
         const size_type ndim;               // number of dimensions for grid
         const Index *dimvec;                // num. coords along each dimension
@@ -27,43 +27,74 @@ class RookNeighborhood {
         size_type dim_cur;                  // current dimension iterator is on
         bool onLwr;                         // true when iterator is on lower
 
+        Index translateCoord(const Index&, const size_type, const int);
+
     public:
 
         RookNeighborhood(const Index &dims) : ndim(dims.size()), dimvec(&dims) {
             lwr_nbr.reserve(ndim);
             upr_nbr.reserve(ndim);
         }
+
         void setCenter(const Index&);
         size_type neighborhoodSize() { return nnbrs; }
         double logNeighborhoodSize() { return log_nnbrs; }
         Index nextNeighbor();
 
+        virtual bool inDomain(const Index&);
+
 };
+
+template<typename size_type, typename Index>
+bool RookNeighborhood<size_type, Index>::inDomain(const Index& coord) {
+    // verify coordinate is within bounds for each dimension
+    for(size_type i=0; i < ndim; ++i) {
+        if(coord[i] < 0) {
+            return false;
+        }
+        if(coord[i] > (*dimvec)[i] - 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename size_type, typename Index>
+Index RookNeighborhood<size_type, Index>::translateCoord(const Index& coord,
+                                                         const size_type dim,
+                                                         const int value) {
+    // shift the coordinate by "value" units in dimension "dim"
+    Index nbr = coord;
+    nbr[dim] += value;
+    return nbr;
+}
 
 template<typename size_type, typename Index>
 void RookNeighborhood<size_type, Index>::setCenter(const Index& coord) {
     // keep a copy of the neighborhood center
     center = coord;
-    // pre-explore the neighborhood
+
+    // reset neighborhood size
     nnbrs = 0;
-    for(size_type i = 0; i < ndim; ++i) {
-        if(center[i] == 0) {
-            lwr_nbr[i] = false;
-            upr_nbr[i] = true;
-            nnbrs++;
-        } else if(center[i] == (*dimvec)[i] - 1) {
-            lwr_nbr[i] = true;
-            upr_nbr[i] = false;
-            nnbrs++;
-        } else if(center[i] > 0 && center[i] < (*dimvec)[i] - 1) {
-            lwr_nbr[i] = true;
-            upr_nbr[i] = true;
-            nnbrs += 2;
-        } else {
-            nnbrs = 0;
-            break;
+
+    // pre-explore the neighborhood
+    if(inDomain(center)) {
+        for(size_type i = 0; i < ndim; ++i) {
+            if(inDomain(translateCoord(center, i, -1))) {
+                lwr_nbr[i] = true;
+                nnbrs++;
+            } else {
+                lwr_nbr[i] = false;
+            }
+            if(inDomain(translateCoord(center, i, 1))) {
+                upr_nbr[i] = true;
+                nnbrs++;
+            } else {
+                upr_nbr[i] = false;
+            }
         }
     }
+
     // update constants
     log_nnbrs = log(nnbrs);
     // initialize neighborhood counters
@@ -80,18 +111,14 @@ Index RookNeighborhood<size_type, Index>::nextNeighbor() {
     } else if(dim_cur < ndim) {
         if(onLwr) {
             if(lwr_nbr[dim_cur]) {
-                Index nbr = center;
-                nbr[dim_cur++] -= 1;
-                return nbr;
+                return translateCoord(center, dim_cur++, -1);
             } else {
                 dim_cur++;
                 return nextNeighbor();
             }
         } else {
             if(upr_nbr[dim_cur]) {
-                Index nbr = center;
-                nbr[dim_cur++] += 1;
-                return nbr;
+                return translateCoord(center, dim_cur++, 1);
             } else {
                 dim_cur++;
                 return nextNeighbor();
