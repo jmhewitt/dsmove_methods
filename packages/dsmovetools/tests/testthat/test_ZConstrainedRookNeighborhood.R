c41ce6d829d34cb2210d469d77072abd80fff283
jmhewitt <joshua.hewitt@duke.edu>
2021-03-09 15:56:17 -0700

    test to validate height-constrained rook neighborhood on 3D surface

--- packages/dsmovetools/tests/testthat/test_ZConstrainedRookNeighborhood.R
+++ packages/dsmovetools/tests/testthat/test_ZConstrainedRookNeighborhood.R
@@ -48,3 +48,55 @@ test_that('Validating constrained 2D rook neighborhoods on a grid', {
   )
   
 })
+
+test_that('Validating constrained 3D rook neighborhoods on a grid', {
+  
+  dims = c(13, 13, 16)
+  
+  zvals = 1:16
+  
+  zfield = matrix(0, nrow = dims[1], ncol = dims[2])
+
+  # raise the 3D space's vertical surface by placing "mountains" where the
+  # complex 2D spatial domain is not valid, here for loc's near the edge and
+  # at the average zval height
+  coords = expand.grid(1:dims[1], 1:dims[2])
+  for(i in 1:nrow(coords)) {
+    if(sum(coords[i,]) < 7) {
+      zfield[i] = mean(zvals) + 1e-10
+    }
+  }
+  
+  # invalid location, height is too low
+  expect_identical(
+    TestZConstrainedRookNeighborhood(dims = dims, x = c(1,1,7), 
+                                     zfield = as.numeric(zfield), 
+                                     zvals = zvals),
+    matrix(0, ncol = 3, nrow = 1)[-1, , drop = FALSE]
+  )
+  
+  # all valid locations, neighborhood entirely above height constraints
+  expect_identical(
+    TestZConstrainedRookNeighborhood(dims = dims, x = c(1,1,9), 
+                                     zfield = as.numeric(zfield), 
+                                     zvals = zvals),
+    TestRookNeighborhood(dims = dims, x = c(1,1,9))
+  )
+  
+  # point along height boundary
+  expect_identical(
+    TestZConstrainedRookNeighborhood(dims = dims, x = c(1,1,8), 
+                                     zfield = as.numeric(zfield), 
+                                     zvals = zvals),
+    TestRookNeighborhood(dims = dims, x = c(1,1,8))[-3,]
+  )
+  
+  # unconstrained points
+  expect_identical(
+    TestZConstrainedRookNeighborhood(dims = dims, x = c(7,7,0), 
+                                     zfield = as.numeric(zfield), 
+                                     zvals = zvals),
+    TestRookNeighborhood(dims = dims, x = c(7,7,0))
+  )
+  
+})
