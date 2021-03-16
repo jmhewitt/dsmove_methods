32dffaef73fb8a6766eed5bbea8e67343d2faff7
jmhewitt <joshua.hewitt@duke.edu>
2021-03-16 11:30:13 -0600

    update ctds.observe method to handle subsetting coordinate observations vs node id's

--- packages/dsmovetools/R/ctds.observe.R
+++ packages/dsmovetools/R/ctds.observe.R
@@ -8,13 +8,23 @@
 #' @param t.obs Times at which the trajectory should be observed
 #' 
 #' 
+#' @example examples/ctds.quicksim.R
+#' 
 #' @export
 #' 
 ctds.observe = function(states, times, t.obs) {
   tind = findInterval(t.obs, times)
   
+  if(inherits(states, 'matrix')) {
+    state_list = apply(states, 2, as.list)
+  } else if(inherits(states, 'numeric')) {
+    state_list = as.list(list(states))
+  }
+  
   res = list(
-    states = states[tind],
+    states = sapply(state_list, function(x) {
+      as.numeric(x[tind])
+    }),
     times = t.obs
   )
   
--- packages/dsmovetools/examples/ctds.quicksim.R
+++ packages/dsmovetools/examples/ctds.quicksim.R
@@ -1,15 +1,28 @@
+# simulation transition rate parameter
 beta_loc = 1
 
+# simulate trajectory
 x = ctds.quicksim(dims = c(100, 100), beta_loc = beta_loc, beta_ar = 1, 
                   v0 = c(50,50), t0 = 0, tf = 1e2, max.steps = 1e4, 
                   v0.last = c(49,50))
 
-# view locations in 2D 
+# observe trajectory, making sure to remove v0.last from ctds.observe input
+tobs = seq(from = x$times[1], to = tail(x$times, 1), length.out = 100)
+x.obs = ctds.observe(states = x$states[-1,], times = x$times, t.obs = tobs)
+
+# view simulated locations in 2D 
 plot(x$states[,1], x$states[,2])
-# view progression in 1D, removing v0.last from plot
+# highlight observations
+points(x.obs$states[,1], x.obs$states[,2], col = 2, pch = 4)
+
+# view progression in 1D
 par(mfrow = c(2,1))
-plot(x$times, x$states[-1,1], type = 'l', xlab = 'Time', ylab = 'x')
-plot(x$times, x$states[-1,2], type = 'l', xlab = 'Time', ylab = 'y')
+plot(stepfun(x = x$times, y = x$states[,1]), pch = '.',
+     xlab = 'Time', ylab = 'x', main = '')
+points(x.obs$times, x.obs$states[,1], col = 2)
+plot(stepfun(x = x$times, y = x$states[,2]), pch = '.',
+     xlab = 'Time', ylab = 'y', main = '')
+points(x.obs$times, x.obs$states[,2], col = 2)
 
 # look at density of durations
 par(mfrow = c(1,1))
