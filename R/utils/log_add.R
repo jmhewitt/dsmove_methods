71c4c48e523f699d917b8e29db3e1d1d0f9f4010
jmhewitt <joshua.hewitt@duke.edu>
2021-03-24 12:40:48 -0600

    extending log-based addition capabilities

--- R/utils/log_add.R
+++ R/utils/log_add.R
@@ -1,4 +1,6 @@
 log_add = function(log_a, log_b) {
+  # evaluate log(c) = log(a + b) when given log(a) and log(b)
+  
   x = log_a - log_b
   exp_x = exp(x)
   if(is.infinite(exp_x)) {
@@ -8,4 +10,17 @@ log_add = function(log_a, log_b) {
     return(log_b)
   }
   return(log_b + log(1 + exp_x))
+}
+
+log_sum = function(x) {
+  # evaluate log(c) = log(x1 + ... + xn) when given log(x1), ..., log(xn)
+  
+  log_res = x[1]
+  len = length(x)
+  if(len > 1) {
+    for(i in 2:len) {
+      log_res = log_add(log_res, x[i])
+    }
+  }
+  log_res
 }
