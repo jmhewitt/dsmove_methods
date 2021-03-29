8ff311322d81e478fbbca46692ded773fcfb1498
jmhewitt <joshua.hewitt@duke.edu>
2021-03-29 14:24:40 -0600

    add warnings to log_add conditions

--- R/utils/log_add.R
+++ R/utils/log_add.R
@@ -4,9 +4,11 @@ log_add = function(log_a, log_b) {
   x = log_a - log_b
   exp_x = exp(x)
   if(is.infinite(exp_x)) {
+    warning('exp(log(a) - log(b)) is infinite')
     return(log_a)
   }
   if(exp_x == 0) {
+    warning('exp(log(a) - log(b)) is 0')
     return(log_b)
   }
   return(log_b + log(1 + exp_x))
