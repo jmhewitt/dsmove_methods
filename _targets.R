f3fff690f21c364b9581dadaa6561269222aa89d
jmhewitt <joshua.hewitt@duke.edu>
2021-04-21 12:08:02 -0600

    add package support for hanks model comparison

--- _targets.R
+++ _targets.R
@@ -15,7 +15,7 @@ tar_option_set(
   # packages = c('dplyr', 'lubridate', 'ggplot2', 'ggthemes', 'stringr', 
   #              'nimble', 'expm', 'pryr', 'suncalc', 'tarchetypes', 'coda',
   #              'tidyr', 'future', 'future.batchtools', 'viridis'),
-  packages = c('dsmovetools'),
+  packages = c('dsmovetools', 'ctmcmove'),
   imports = 'dsmovetools',
   deployment = 'main'
 )
