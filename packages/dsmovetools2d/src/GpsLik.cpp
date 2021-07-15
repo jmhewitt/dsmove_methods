3e96b0f6df13d84c13f1fc6caf2b1770162e312a
jmhewitt <joshua.hewitt@duke.edu>
2021-07-15 11:08:38 -0600

    initial transfer of GPS likelihoods

---
+++ packages/dsmovetools2d/src/GpsLik.cpp
@@ -0,0 +1,109 @@
+#include "GpsLik.h"
+
+void GpsLikBase::parameterizeDistribution(
+            double lon_ctr, double lat_ctr, double semi_major, double semi_minor,
+            double orientation) {
+
+    if(std::isnan(lon_ctr)) {
+        na_obs = true;
+        return;
+    } else {
+        na_obs = false;
+    }
+
+    // Implements "Materials and Methods" from McClintock et al. (2015)
+
+    double Mtsq_half = std::pow(semi_major, 2) / 2;
+    double mtsq_half = std::pow(semi_minor, 2) / 2;
+
+    double c = (90 - orientation) / 180 * M_PI;
+    double cos_c = std::cos(c);
+    double cos2_c = std::pow(cos_c, 2);
+    double sin_c = std::sin(c);
+    double sin2_c = std::pow(sin_c, 2);
+
+    mu_lon = lon_ctr;
+    mu_lat = lat_ctr;
+
+    sd_lon = std::sqrt(Mtsq_half * sin2_c + mtsq_half * cos2_c);
+    sd_lat = std::sqrt(Mtsq_half * cos2_c + mtsq_half * sin2_c);
+    rho = (Mtsq_half - mtsq_half) * cos_c * sin_c / sd_lat / sd_lon;
+
+    rhosq_c = 1 - std::pow(rho, 2);
+
+    // log-normalizing constant, including rescaling for truncation
+    lcst = - std::log(2 * M_PI * sd_lon * sd_lat) - 0.5 * std::log(rhosq_c) -
+            ltrunc;
+}
+
+double GpsLikBase::ll(double lon, double lat) {
+
+    // flat likelihood when observations are NA (i.e., are uninformative)
+    if(na_obs) {
+        return 0;
+    }
+
+    // convert distances wrt. lon and lat to m; scale wrt. uncertainty
+    double zx = distance_m(lon, mu_lat, mu_lon, mu_lat) / sd_lon;
+    double zy = distance_m(mu_lon, lat, mu_lon, mu_lat) / sd_lat;
+
+    // quadratic form
+    double q = std::pow(zx, 2) - 2 * rho * zx * zy + std::pow(zy, 2);
+
+    // return -Inf if lon/lat are outside the ellipse truncation boundary
+    if(q < qform_thresh) {
+        return - q / 2 / rhosq_c + lcst;
+    } else {
+        return -std::numeric_limits<double>::infinity();
+    }
+}
+
+double GpsLikGridded::ll(unsigned int lon_ind, unsigned int lat_ind) {
+    return GpsLik::ll((*lon_gridvals)[lon_ind],
+                      (*lat_gridvals)[lat_ind]);
+}
+
+double GpsLikBase::distance_m(double lon1, double lat1, double lon2, double lat2) {
+
+    // convert units from degrees to radians
+    lat1 = lat1 * M_PI / 180;
+    lon1 = lon1 * M_PI / 180;
+    lat2 = lat2 * M_PI / 180;
+    lon2 = lon2 * M_PI / 180;
+
+    // Haversine Formula
+    double dlon = lon2 - lon1;
+    double dlat = lat2 - lat1;
+
+    double ans = pow(sin(dlat / 2), 2) +
+                      cos(lat1) * cos(lat2) *
+                      pow(sin(dlon / 2), 2);
+
+    ans = 2 * asin(sqrt(ans));
+
+    // radius of Earth in meters
+    double R = 6378388;
+
+    // Calculate the result
+    return ans * R;
+}
+
+void GpsLik::setLikToObs(unsigned int ind) {
+    parameterizeDistribution((*lon_ctrs)[ind], (*lat_ctrs)[ind],
+                             (*semi_majors)[ind], (*semi_minors)[ind],
+                             (*orientations)[ind]);
+}
+
+// [[Rcpp::export]]
+double GpsLikEval(
+        std::vector<double> obs_lons, std::vector<double> obs_lats,
+        std::vector<double> semi_majors, std::vector<double> semi_minors,
+        std::vector<double> orientations, double alpha, double test_lon,
+        double test_lat, int ind) {
+
+    GpsLik g(alpha, obs_lons, obs_lats, semi_majors, semi_minors, orientations);
+
+    g.setLikToObs(ind);
+
+    return g.ll(test_lon, test_lat);
+}
---
+++ packages/dsmovetools2d/src/GpsLik.h
@@ -0,0 +1,87 @@
+//
+// Created by Joshua Hewitt on 12/14/20.
+//
+
+#include <Rcpp.h>
+
+#ifndef DSMOVETOOLS2D_GPS_LIK_H
+#define DSMOVETOOLS2D_GPS_LIK_H
+
+using namespace Rcpp;
+
+class GpsLikBase {
+
+    private:
+
+        // quick switch to indicate that observations are NA
+        bool na_obs;
+
+        // bivariate normal parameters and constants
+        double mu_lon, mu_lat, sd_lon, sd_lat, rho, rhosq_c, lcst;
+
+        // truncation parameters
+        double qform_thresh, ltrunc;
+
+        // distance between two lon/lat coords, in meters
+        double distance_m(double, double, double, double);
+
+    public:
+
+        GpsLikBase(double alpha) :
+            ltrunc(std::log(1-alpha)),
+            qform_thresh(R::qchisq(1-alpha, 2, 1, 0)) { };
+
+        // log-likelihood for coordinates relative to observation uncertainty
+        double ll(double lon, double lat);
+
+        // parameterize dist'n. using lon/lat/error information
+        void parameterizeDistribution(
+            double lon_ctr, double lat_ctr, double semi_major,
+            double semi_minor, double orientation
+        );
+
+};
+
+class GpsLik : public GpsLikBase {
+
+    private:
+
+        // GPS lon/lat observations with error ellipse information
+        std::vector<double> *lon_ctrs, *lat_ctrs, *semi_majors, *semi_minors,
+            *orientations;
+
+    public:
+
+        GpsLik(double alpha, std::vector<double> &lons,
+               std::vector<double> &lats, std::vector<double> &majors,
+               std::vector<double> &minors, std::vector<double> &orients) :
+                lon_ctrs(&lons), lat_ctrs(&lats), semi_majors(&majors),
+                semi_minors(&minors), orientations(&orients),
+                GpsLikBase(alpha) { };
+
+        // parameterize log-likelihood using an observation, specified via index
+        void setLikToObs(unsigned int);
+
+};
+
+class GpsLikGridded : public GpsLik {
+
+    private:
+
+        // grid definitions
+        std::vector<double> *lon_gridvals, *lat_gridvals;
+
+    public:
+
+        GpsLikGridded(double alpha, std::vector<double> &lons,
+          std::vector<double> &lats, std::vector<double> &majors,
+          std::vector<double> &minors, std::vector<double> &orients,
+          std::vector<double> &lon_grid, std::vector<double> &lat_grid) :
+                GpsLik(alpha, lons, lats, majors, minors, orients),
+                lon_gridvals(&lon_grid), lat_gridvals(&lat_grid) { };
+
+        double ll(unsigned int lon_ind, unsigned int lat_ind);
+
+};
+
+#endif //DSMOVETOOLS2D_GPS_LIK_H
