9599bb5d709dae15d61d9cfcb28ca409e4259e71
jmhewitt <joshua.hewitt@duke.edu>
2021-09-02 09:21:09 -0600

    compare posterior for alternate parameterization

--- tmp_post_whale.R
+++ tmp_post_whale.R
@@ -14,40 +14,86 @@ whale_ll_srtm30_filtered = readRDS('dcc/whale_ll_srtm30_filtered_alt_orientation
 # rescale speed grid to align with average meters traveled per cell transition
 whale_ll_srtm30_filtered$speed = whale_ll_srtm30_filtered$speed * 623.7801 / 1187.295
 
+# shrink beta0 toward the speed there should be if betaAR = 0. ideally it might 
+# make more sense to be able to shrink toward a known speed range, independently
+# of making assumptions about betaAR
+log(1.2) - log(4 * 623.7801)
+
 
 source('R/utils/log_add.R')
 source('R/utils/distribution_parameters.R')
 
 
 priors = list(
-  # speed = gamma.param(mu = .5, sd = .5)
-  speed = gamma.param(mu = 1, sd = 1)
+  # speed = gamma.param(mu = .5, sd = .5),
+  speed = gamma.param(mu = 1, sd = 1),
   # speed = c(shape = 2, rate = 1)
+  betaAR = c(mean = 0, sd = 1e2),
+  beta0_hanks = c(mean = 0, sd = 1e2)
 )
 
-lC = log_sum(
+# one way to potentially help set the speed prior
+qgamma(.99, shape = priors$speed['shape'], rate = priors$speed['rate'])
+
+#
+# back-transform hanks model parameterizations
+#
+
+# append induced hanks location parameter
+whale_ll_srtm30_filtered$beta0_hanks = 
+  log(whale_ll_srtm30_filtered$speed) -
+  log(623.7801) -
+  log(2 + exp(-whale_ll_srtm30_filtered$betaAR) + 
+        exp(whale_ll_srtm30_filtered$betaAR) )
+
+whale_ll_srtm30_filtered$lp_hanks = 
+  # likelihood
   whale_ll_srtm30_filtered$ll + 
+  # prior
+  dnorm(x = whale_ll_srtm30_filtered$beta0_hanks, 
+        mean = priors$beta0_hanks['mean'], 
+        sd = priors$beta0_hanks['sd'], log = TRUE) +
+  dnorm(x = whale_ll_srtm30_filtered$betaAR,
+        mean = priors$betaAR['mean'], 
+        sd = priors$betaAR['sd'], log = TRUE) -
+  # jacobian for transformation
+  log(whale_ll_srtm30_filtered$speed)
+
+whale_ll_srtm30_filtered$lp = 
+  # likelihood
+  whale_ll_srtm30_filtered$ll + 
+  # prior
   dgamma(x = whale_ll_srtm30_filtered$speed,
          shape = priors$speed['shape'],
          rate = priors$speed['rate'],
-         log = TRUE) +
+         log = TRUE) + 
+  dnorm(x = whale_ll_srtm30_filtered$betaAR,
+        mean = priors$betaAR['mean'], 
+        sd = priors$betaAR['sd'], log = TRUE)
+  
+
+lC = log_sum(
+  whale_ll_srtm30_filtered$lp + 
   log(diff(unique(whale_ll_srtm30_filtered$speed))[1]) + 
   log(diff(unique(whale_ll_srtm30_filtered$betaAR))[1])
 ) 
 
+lC_hanks = log_sum(
+  whale_ll_srtm30_filtered$lp_hanks + 
+    log(diff(unique(whale_ll_srtm30_filtered$speed))[1]) + 
+    log(diff(unique(whale_ll_srtm30_filtered$betaAR))[1])
+) 
+
 
 pl = ggplot(whale_ll_srtm30_filtered %>% 
-              mutate(ll = ll + 
-                       dgamma(x = whale_ll_srtm30_filtered$speed,
-                              shape = priors$speed['shape'],
-                              rate = priors$speed['rate'],
-                              log = TRUE)
+              mutate(ll = lp_hanks - lC_hanks
               ) %>% 
-          filter( ll > -800  ),
+          filter( ll > -20  ),
        aes(x = speed, y = betaAR, fill = ll, z = ll)) + 
+  # geom_tile(height = 0.3157895, width = 0.3139539) +
   geom_raster() + 
-  geom_contour2(col = 'grey50') +
-  geom_text_contour(col = 'grey90') +
+  geom_contour2(col = 'grey60') + 
+  geom_text_contour(col = 'grey90') + 
   theme_few() + 
   scale_fill_viridis(direction = -1) + 
   ggtitle('Zc093 prior mean = 1, sd = 1')
@@ -60,9 +106,7 @@ speed_marginal = whale_ll_srtm30_filtered %>%
   group_by(speed) %>% 
   summarise(
     lp = log_sum(
-      ll +
-        dgamma(x = speed, shape = priors$speed['shape'],
-               rate = priors$speed['rate'], log = TRUE) +
+      lp + 
         log(diff(unique(whale_ll_srtm30_filtered$betaAR))[1]) -
         lC
     ) 
@@ -147,6 +191,12 @@ pl = ggplot(speed_marginal, aes(x = speed, y = exp(lp))) +
   stat_function(
     fun = function(x) { dgamma(x = x, shape = priors$speed['shape'],
                                rate = priors$speed['rate'], log = FALSE)},
+    # fun = function(x) {
+    #   # prior assumes betaAR = 0, which is a bit of a kludge for the transformation
+    #   beta0 = log(x) - log(623.7801) - log(4)
+    #   dnorm(x = beta0, mean = priors$beta0_hanks['mean'],
+    #         sd = priors$beta0_hanks['sd'], log = FALSE) / x
+    # },
                 lty = 3, geom = 'line') +
   geom_line() + 
   theme_few() + 
@@ -173,3 +223,9 @@ pl = whale_ll_srtm30_filtered %>%
 pl
 
 ggsave(pl, filename = 'Zc073_fastloc_mu_3_sd_1_betaAR.pdf')
+
+
+# CONCLUSION for comparison of 2 and 4, in speed space
+#  - for weak priors, the posteriors between the two parameterizations are 
+#    relatively similar (wrt the speed parameter), but it is much easier to 
+#    interpret the (mu,\beta1) prior.
